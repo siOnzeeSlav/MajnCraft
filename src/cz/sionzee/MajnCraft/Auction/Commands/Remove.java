7f087b42972876df3e67981293ff20c36c8fe21d
sionzee@frelania.eu <sionzee@frelania.eu>
2014-02-21 14:31:13 +0100

    Added command Remove

---
+++ src/cz/sionzee/MajnCraft/Auction/Commands/Remove.java
@@ -0,0 +1,15 @@
+package cz.sionzee.MajnCraft.Auction.Commands;
+
+import org.bukkit.entity.Player;
+
+import cz.sionzee.MajnCraft.Auction.Interfaces.ICommand;
+
+public class Remove implements ICommand {
+
+    @Override
+    public boolean onCommand(Player p, String[] args) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+}
