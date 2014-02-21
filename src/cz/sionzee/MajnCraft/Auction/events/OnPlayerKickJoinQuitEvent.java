ab9bb2932d27a6f0fde93d1aa4e3a77a9cfc4548
sionzee@frelania.eu <sionzee@frelania.eu>
2014-02-21 14:53:21 +0100

    Added On(Join,Kick,Quit) events

---
+++ src/cz/sionzee/MajnCraft/Auction/events/OnPlayerKickJoinQuitEvent.java
@@ -0,0 +1,25 @@
+package cz.sionzee.MajnCraft.Auction.events;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerKickEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+public class OnPlayerKickJoinQuitEvent {
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event) {
+
+    }
+
+    @EventHandler
+    public void onKick(PlayerKickEvent event) {
+
+    }
+
+    @EventHandler
+    public void onQuit(PlayerQuitEvent event) {
+
+    }
+
+}
