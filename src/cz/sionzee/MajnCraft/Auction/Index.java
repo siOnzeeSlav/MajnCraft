f67670e31061ac00feb70be7f126a0d572fd47a7
sionzee@frelania.eu <sionzee@frelania.eu>
2014-02-22 21:12:57 +0100

    Added vault support && checking if is enabled

--- src/cz/sionzee/MajnCraft/Auction/Index.java
+++ src/cz/sionzee/MajnCraft/Auction/Index.java
@@ -1,27 +1,37 @@
 package cz.sionzee.MajnCraft.Auction;
 
-import java.util.HashMap;
-
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import cz.sionzee.MajnCraft.Auction.Managers.ConfigurationManager;
 import cz.sionzee.MajnCraft.Auction.Managers.DatabaseManager;
+import cz.sionzee.MajnCraft.Auction.Managers.EconomyManager;
+import cz.sionzee.MajnCraft.Auction.Managers.VaultManager;
+import cz.sionzee.MajnCraft.Auction.events.OnPlayerKickJoinQuitEvent;
 
 public class Index extends JavaPlugin {
 
-    public static HashMap<String, MajnCraftPlayer> players;
     static Index i;
 
     @Override
     public void onEnable() {
         i = this;
-        players = new HashMap<String, MajnCraftPlayer>();
         ConfigurationManager.initialize();
         if (!DatabaseManager.initialize()) {
             this.setEnabled(false);
         }
         getCommand("ma").setExecutor(new CommandHandler());
         CommandHandler.initialize();
+        EconomyManager.initialize();
+        if (EconomyManager.isEnabled()) {
+            Bukkit.getPluginManager().registerEvents(new OnPlayerKickJoinQuitEvent(), this);
+        } else {
+            if (Bukkit.getPluginManager().getPlugin("Vault") == null || !Bukkit.getPluginManager().getPlugin("Vault").isEnabled()) {
+                this.setEnabled(false);
+            } else {
+                VaultManager.initialize();
+            }
+        }
     }
 
     @Override
---
+++ src/cz/sionzee/MajnCraft/Auction/Managers/VaultManager.java
@@ -0,0 +1,37 @@
+package cz.sionzee.MajnCraft.Auction.Managers;
+
+import net.milkbowl.vault.chat.Chat;
+import net.milkbowl.vault.economy.Economy;
+import net.milkbowl.vault.permission.Permission;
+
+import org.bukkit.plugin.RegisteredServiceProvider;
+
+import cz.sionzee.MajnCraft.Auction.Index;
+
+public class VaultManager {
+
+    static Permission permission = null;
+    static Economy economy = null;
+    static Chat chat = null;
+
+    public static void initialize() {
+
+        Index i = Index.getInstance();
+
+        RegisteredServiceProvider<Permission> permissionProvider = i.getServer().getServicesManager().getRegistration(net.milkbowl.vault.permission.Permission.class);
+        if (permissionProvider != null) {
+            permission = permissionProvider.getProvider();
+        }
+
+        RegisteredServiceProvider<Chat> chatProvider = i.getServer().getServicesManager().getRegistration(net.milkbowl.vault.chat.Chat.class);
+        if (chatProvider != null) {
+            chat = chatProvider.getProvider();
+        }
+
+        RegisteredServiceProvider<Economy> economyProvider = i.getServer().getServicesManager().getRegistration(net.milkbowl.vault.economy.Economy.class);
+        if (economyProvider != null) {
+            economy = economyProvider.getProvider();
+        }
+    }
+
+}
