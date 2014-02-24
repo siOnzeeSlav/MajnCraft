5f1466450c69c088f987e539a080268001ba10a9
sionzee@frelania.eu <sionzee@frelania.eu>
2014-02-25 00:25:03 +0100

    Added support for unload/load

--- src/cz/sionzee/MajnCraft/Auction/Index.java
+++ src/cz/sionzee/MajnCraft/Auction/Index.java
@@ -1,13 +1,13 @@
 package cz.sionzee.MajnCraft.Auction;
 
-import org.bukkit.Bukkit;
-import org.bukkit.plugin.java.JavaPlugin;
-
 import cz.sionzee.MajnCraft.Auction.Managers.ConfigurationManager;
 import cz.sionzee.MajnCraft.Auction.Managers.DatabaseManager;
 import cz.sionzee.MajnCraft.Auction.Managers.EconomyManager;
 import cz.sionzee.MajnCraft.Auction.Managers.VaultManager;
 import cz.sionzee.MajnCraft.Auction.events.OnPlayerKickJoinQuitEvent;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
 
 public class Index extends JavaPlugin {
 
@@ -25,6 +25,10 @@ public class Index extends JavaPlugin {
         EconomyManager.initialize();
         if (EconomyManager.isEnabled()) {
             Bukkit.getPluginManager().registerEvents(new OnPlayerKickJoinQuitEvent(), this);
+            for (Player player : Bukkit.getOnlinePlayers()) {
+                String playerName = player.getName();
+                EconomyManager.addPlayer(new MajnCraftPlayer(player, EconomyManager.getThisMoney(playerName)), playerName);
+            }
         } else {
             if (Bukkit.getPluginManager().getPlugin("Vault") == null || !Bukkit.getPluginManager().getPlugin("Vault").isEnabled()) {
                 this.setEnabled(false);
@@ -36,7 +40,10 @@ public class Index extends JavaPlugin {
 
     @Override
     public void onDisable() {
-
+        for (Player player : Bukkit.getOnlinePlayers()) {
+            String playerName = player.getName();
+            EconomyManager.removePlayer(playerName);
+        }
     }
 
     public static Index getInstance() {
