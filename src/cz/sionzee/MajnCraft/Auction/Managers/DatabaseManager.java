077267832873356b1918c786d73276c9d373977d
sionzee@frelania.eu <sionzee@frelania.eu>
2014-02-25 00:31:11 +0100

    Added support for prepared statament && repaired sql syntax

--- src/cz/sionzee/MajnCraft/Auction/Managers/DatabaseManager.java
+++ src/cz/sionzee/MajnCraft/Auction/Managers/DatabaseManager.java
@@ -1,17 +1,11 @@
 package cz.sionzee.MajnCraft.Auction.Managers;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
-
-import org.bukkit.configuration.Configuration;
-
-import com.mongodb.DB;
-import com.mongodb.MongoClient;
-
 import cz.sionzee.MajnCraft.Auction.ErrorMessages;
 import cz.sionzee.MajnCraft.Auction.Log;
+import org.bukkit.configuration.Configuration;
+
+import java.sql.*;
+import java.util.HashMap;
 
 public class DatabaseManager {
 
@@ -19,20 +13,36 @@ public class DatabaseManager {
     static Statement sta;
     static String database;
     static String tablePrefix;
+    static String username;
+    static String password;
+    static String host;
+    static int port;
 
-    static MongoClient mongoClient;
-    static DB db;
+    //static MongoClient mongoClient;
+    //static DB db;
+
+    static HashMap<String, PreparedStatement> store;
+
+    public static String getDatabaseName() {
+        return database;
+    }
+
+    public static String getTablePrefix() {
+        return tablePrefix;
+    }
 
     public static boolean initialize() {
 
+        store = new HashMap<String, PreparedStatement>();
+
         Configuration config = ConfigurationManager.getConfig();
 
-        String username = config.getString("Database.Username");
-        String password = config.getString("Database.Password");
+        username = config.getString("Database.Username");
+        password = config.getString("Database.Password");
         database = config.getString("Database.Database");
-        String host = config.getString("Database.Host");
+        host = config.getString("Database.Host");
         tablePrefix = config.getString("Database.TablesPrefix");
-        int port = config.getInt("Database.Port");
+        port = config.getInt("Database.Port");
 
         if (username.length() == 0 || password.length() == 0 || database.length() == 0 || host.length() == 0 || tablePrefix.length() == 0 || port == 0) {
             Log.$(ErrorMessages.NODATABASECONFIG);
@@ -55,14 +65,7 @@ public class DatabaseManager {
         /** Successfull in db 
         */
 
-        try {
-            con = DriverManager.getConnection(String.format("jdbc:mysql://{0}:{1}/{2}", host, port, database), username, password);
-        } catch (SQLException e) {
-            Log.$(ErrorMessages.DATABASEWRONGPARAMETERS);
-            Log.$("ERROR: " + e.getMessage());
-            e.printStackTrace();
-            return false;
-        }
+        openConnection();
 
         try {
             sta = con.createStatement();
@@ -74,11 +77,21 @@ public class DatabaseManager {
         return true;
     }
 
+    private static void openConnection() {
+        try {
+            con = DriverManager.getConnection(String.format("jdbc:mysql://%s:%d/%s", host, port, database), username, password);
+        } catch (SQLException e) {
+            Log.$(ErrorMessages.DATABASEWRONGPARAMETERS);
+            Log.$("ERROR: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
     public static boolean checkTables() {
         try {
 
             if (EconomyManager.isEnabled()) {
-                sta.executeQuery(String.format("CREATE TABLE IF NOT EXISTS `{0}`.`{1}` ( `id` INT NOT NULL AUTO_INCREMENT, `playername` VARCHAR(32) NULL, `money` INT NULL DEFAULT 0, `access` TINYINT NULL DEFAULT 1, PRIMARY KEY (`id`)) ENGINE = InnoDB", database, tablePrefix + "players"));
+                sta.executeQuery(String.format("CREATE TABLE IF NOT EXISTS `%s`.`%s` ( `id` INT NOT NULL AUTO_INCREMENT, `playername` VARCHAR(32) NULL, `money` INT NULL DEFAULT 0, `access` TINYINT NULL DEFAULT 1, PRIMARY KEY (`id`)) ENGINE = InnoDB", database, tablePrefix + "players"));
             }
 
             return true;
@@ -88,6 +101,55 @@ public class DatabaseManager {
             e.printStackTrace();
             return false;
         }
+    }
+
+    public static ResultSet executeQuery(String quera) {
+
+        try {
+            if (con.isClosed())
+                openConnection();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            if (sta.isClosed())
+                sta = con.createStatement();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        if (quera.toLowerCase().startsWith("select")) {
+            try {
+                return sta.executeQuery(quera);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        } else {
+            try {
+                sta.executeUpdate(quera);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    public static PreparedStatement getPreparedQuery(String name) {
+        return store.get(name);
+    }
+
+
+    public static void preparedQuery(String ind, String quera) {
+        PreparedStatement ps = null;
+
+        try {
+            ps = con.prepareStatement(quera);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        store.put(ind, ps);
     }
 
 }
