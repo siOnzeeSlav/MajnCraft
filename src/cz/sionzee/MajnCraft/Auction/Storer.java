6db5786373e0f316abe0a63dc38579e44bac5137
sionzee@frelania.eu <sionzee@frelania.eu>
2014-02-25 00:24:15 +0100

    File where are stored 'something' values

---
+++ src/cz/sionzee/MajnCraft/Auction/Storer.java
@@ -0,0 +1,12 @@
+package cz.sionzee.MajnCraft.Auction;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by siOnzeeSlav on 24.2.14.
+ */
+public class Storer {
+
+    public static List<String> playernames = new ArrayList<String>();
+}
