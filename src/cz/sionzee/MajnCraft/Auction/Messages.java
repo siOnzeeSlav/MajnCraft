ad5b3b39e4eee15c58a8c2c40db4dd23ea48eeaa
sionzee@frelania.eu <sionzee@frelania.eu>
2014-02-25 01:08:30 +0100

    Added more translation lines

--- src/cz/sionzee/MajnCraft/Auction/Messages.java
+++ src/cz/sionzee/MajnCraft/Auction/Messages.java
@@ -13,6 +13,12 @@ public class Messages {
     public static void loadDefault() {
         messages.put("SIGN_OPEN", "Open");
         messages.put("SIGN_PLACED", "You're successfully placed auction sign.");
+        messages.put("AUCTION_MENU_COLLECT", "Collect items");
+        messages.put("AUCTION_MENU_COLLECT_LORE", "Pick up your stuff that you bought.");
+        messages.put("AUCTION_MENU_GIVEOVER", "Give over items");
+        messages.put("AUCTION_MENU_GIVEOVER_LORE", "Give the things you want to sell.");
+        messages.put("AUCTION_MENU_EXIT", "Exit");
+        messages.put("AUCTION_MENU_EXIT_LORE", "Exit the auction menu.");
     }
 
     public static String get(String name) {
