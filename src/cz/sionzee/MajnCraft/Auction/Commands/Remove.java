ec3c62ed336905b4b44c095fc40ad85b9c5cf12b
sionzee@frelania.eu <sionzee@frelania.eu>
2014-02-25 04:38:34 +0100

    Finished cmd Remove

--- src/cz/sionzee/MajnCraft/Auction/Commands/Remove.java
+++ src/cz/sionzee/MajnCraft/Auction/Commands/Remove.java
@@ -1,14 +1,15 @@
 package cz.sionzee.MajnCraft.Auction.Commands;
 
-import org.bukkit.entity.Player;
-
 import cz.sionzee.MajnCraft.Auction.Interfaces.ICommand;
+import cz.sionzee.MajnCraft.Auction.Storer;
+import org.bukkit.entity.Player;
 
 public class Remove implements ICommand {
 
     @Override
     public boolean onCommand(Player p, String[] args) {
-        // TODO Auto-generated method stub
+        if (!Storer.playernames.contains(p.getName()))
+            Storer.playernamesremove.add(p.getName());
         return false;
     }
 
