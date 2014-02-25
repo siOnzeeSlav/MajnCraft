f05dc47c626a0af5a5f3cfabe1a2a09e715f7b02
sionzee@frelania.eu <sionzee@frelania.eu>
2014-02-25 01:44:57 +0100

    Added lines for translation

--- src/cz/sionzee/MajnCraft/Auction/Messages.java
+++ src/cz/sionzee/MajnCraft/Auction/Messages.java
@@ -19,6 +19,12 @@ public class Messages {
         messages.put("AUCTION_MENU_GIVEOVER_LORE", "Give the things you want to sell.");
         messages.put("AUCTION_MENU_EXIT", "Exit");
         messages.put("AUCTION_MENU_EXIT_LORE", "Exit the auction menu.");
+        messages.put("ECONOMY_MONEY", "You're have %d money.");
+        messages.put("ECONOMY_PAY_NOTFOUND", "Player %s not found.");
+        messages.put("ECONOMY_PAY_NOMONEY", "You don't have money!");
+        messages.put("ECONOMY_PAY_NEED", "To send money you need %d.");
+        messages.put("ECONOMY_PAY_SUCCESSFULL", "You're successfull sended %d money.");
+        messages.put("ECONOMY_PAY_COLLECTED", "You're collected %d money from %s.");
     }
 
     public static String get(String name) {
