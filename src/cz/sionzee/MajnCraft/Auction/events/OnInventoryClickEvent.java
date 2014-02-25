6d31c6a48d3606da83131b67b7f2a51c70610e6c
sionzee@frelania.eu <sionzee@frelania.eu>
2014-02-25 22:11:15 +0100

    Renamed invetories

--- src/cz/sionzee/MajnCraft/Auction/events/OnInventoryClickEvent.java
+++ src/cz/sionzee/MajnCraft/Auction/events/OnInventoryClickEvent.java
@@ -1,5 +1,6 @@
 package cz.sionzee.MajnCraft.Auction.events;
 
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -15,24 +16,36 @@ public class OnInventoryClickEvent implements Listener {
 
     @EventHandler
     public void onInventoryClick(InventoryClickEvent event) {
+        Player player = (Player) event.getWhoClicked();
+        String playerName = player.getName();
         Inventory inventory = event.getInventory();
-        if (inventory.getName().equals("Auction Menu")) {
-            Player player = (Player) event.getWhoClicked();
-            ItemStack clicked = event.getCurrentItem();
-
-            switch (clicked.getType()) {
-                case HOPPER:
-
-                    break;
-                case WOODEN_DOOR:
-                    player.closeInventory();
-                    break;
-
-                case CHEST:
-
-                    break;
-            }
+        if (inventory.getName().equals(playerName + " HOPPER")) {
             event.setCancelled(true);
+        }
+
+        if (inventory.getName().equals(playerName + " CHEST")) {
+            event.setCancelled(true);
+        }
+
+        if (inventory.getName().equals("Auction Menu")) {
+            if (event.getCurrentItem() != null) {
+                ItemStack clicked = event.getCurrentItem();
+
+                switch (clicked.getType()) {
+                    case HOPPER:
+                        player.closeInventory();
+                        Bukkit.createInventory(player, 9, player.getName() + " HOPPER");
+                        break;
+                    case WOOD_DOOR:
+                        player.closeInventory();
+                        break;
+
+                    case CHEST:
+                        Bukkit.createInventory(player, 9, player.getName() + " CHEST");
+                        break;
+                }
+                event.setCancelled(true);
+            }
         }
     }
 
