3ad498d8dd88e241497100bacca081c790a1881c
sionzee@frelania.eu <sionzee@frelania.eu>
2014-02-25 03:42:06 +0100

    Fixed error && checking player access to auction

--- src/cz/sionzee/MajnCraft/Auction/Index.java
+++ src/cz/sionzee/MajnCraft/Auction/Index.java
@@ -14,6 +14,8 @@ import org.bukkit.entity.Player;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.sql.SQLException;
+
 public class Index extends JavaPlugin {
 
     static Index i;
@@ -34,7 +36,11 @@ public class Index extends JavaPlugin {
             pm.registerEvents(new OnPlayerKickJoinQuitEvent(), this);
             for (Player player : Bukkit.getOnlinePlayers()) {
                 String playerName = player.getName();
-                EconomyManager.addPlayer(new MajnCraftPlayer(player, EconomyManager.getThisMoney(playerName)), playerName);
+                try {
+                    EconomyManager.addPlayer(new MajnCraftPlayer(player, EconomyManager.getThisMoney(playerName), !DatabaseManager.executeQuery(String.format("SELECT COUNT(*) FROM `%s`.`%s` WHERE `playername`='%s' ENGINE = InnoDB", DatabaseManager.getDatabaseName(), DatabaseManager.getTablePrefix() + "players", playerName)).next()), playerName);
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
             }
         } else {
             if (Bukkit.getPluginManager().getPlugin("Vault") == null || !Bukkit.getPluginManager().getPlugin("Vault").isEnabled()) {
