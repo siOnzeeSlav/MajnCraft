45279ddb0b938149336fc28b13a5459f69df2104
sionzee@frelania.eu <sionzee@frelania.eu>
2014-02-25 01:14:30 +0100

    Registering new events OnInventoryClick, OnPlayerInteract

--- src/cz/sionzee/MajnCraft/Auction/Index.java
+++ src/cz/sionzee/MajnCraft/Auction/Index.java
@@ -4,9 +4,13 @@ import cz.sionzee.MajnCraft.Auction.Managers.ConfigurationManager;
 import cz.sionzee.MajnCraft.Auction.Managers.DatabaseManager;
 import cz.sionzee.MajnCraft.Auction.Managers.EconomyManager;
 import cz.sionzee.MajnCraft.Auction.Managers.VaultManager;
+import cz.sionzee.MajnCraft.Auction.events.OnInventoryClickEvent;
+import cz.sionzee.MajnCraft.Auction.events.OnPlayerInteractEvent;
 import cz.sionzee.MajnCraft.Auction.events.OnPlayerKickJoinQuitEvent;
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Index extends JavaPlugin {
@@ -16,6 +20,7 @@ public class Index extends JavaPlugin {
     @Override
     public void onEnable() {
         i = this;
+        PluginManager pm = Bukkit.getPluginManager();
         ConfigurationManager.initialize();
         if (!DatabaseManager.initialize()) {
             this.setEnabled(false);
@@ -24,7 +29,7 @@ public class Index extends JavaPlugin {
         CommandHandler.initialize();
         EconomyManager.initialize();
         if (EconomyManager.isEnabled()) {
-            Bukkit.getPluginManager().registerEvents(new OnPlayerKickJoinQuitEvent(), this);
+            pm.registerEvents(new OnPlayerKickJoinQuitEvent(), this);
             for (Player player : Bukkit.getOnlinePlayers()) {
                 String playerName = player.getName();
                 EconomyManager.addPlayer(new MajnCraftPlayer(player, EconomyManager.getThisMoney(playerName)), playerName);
@@ -36,6 +41,12 @@ public class Index extends JavaPlugin {
                 VaultManager.initialize();
             }
         }
+        Storer.auctionMenu.setItem(0, Utils.createItemStack(Material.CHEST, Messages.get("AUCTION_MENU_COLLECT"), Messages.get("AUCTION_MENU_COLLECT_LORE")));
+        Storer.auctionMenu.setItem(1, Utils.createItemStack(Material.HOPPER, Messages.get("AUCTION_MENU_GIVEOVER"), Messages.get("AUCTION_MENU_GIVEOVER_LORE")));
+        Storer.auctionMenu.setItem(8, Utils.createItemStack(Material.WOODEN_DOOR, Messages.get("AUCTION_MENU_EXIT"), Messages.get("AUCTION_MENU_EXIT_LORE")));
+        pm.registerEvents(new OnInventoryClickEvent(), this);
+        pm.registerEvents(new OnPlayerInteractEvent(), this);
+
     }
 
     @Override
