a4d6da93bffcfdd56694c88f2621557e2cf02053
sionzee@frelania.eu <sionzee@frelania.eu>
2014-02-25 22:10:50 +0100

    Improvized && fixed onInteractEvent

--- src/cz/sionzee/MajnCraft/Auction/events/OnPlayerInteractEvent.java
+++ src/cz/sionzee/MajnCraft/Auction/events/OnPlayerInteractEvent.java
@@ -2,6 +2,7 @@ package cz.sionzee.MajnCraft.Auction.events;
 
 import cz.sionzee.MajnCraft.Auction.Messages;
 import cz.sionzee.MajnCraft.Auction.Storer;
+import cz.sionzee.MajnCraft.Auction.Utils;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
@@ -27,36 +28,40 @@ public class OnPlayerInteractEvent implements Listener {
 
             Material blockType = block.getType();
 
-            if (blockType == Material.SIGN || blockType == Material.SIGN_POST) {
-                Sign sign = (Sign) block.getState();
-                if (sign.getLine(0).equals("[MineAuction]")) {
-                    if (sign.getLine(1).equals(Messages.get("SIGN_OPEN"))) {
-                        if (p.getOpenInventory() == null)
-                            p.openInventory(Storer.auctionMenu);
+            if (Storer.playernamesremove.contains(playerName)) {
+                Location blockLocation = block.getLocation();
+                if (blockType == Material.SIGN_POST || blockType == Material.WALL_SIGN) {
+                    Sign sign = (Sign) blockLocation.getWorld().getBlockAt(blockLocation).getState();
+                    if (sign.getLine(0).equals("[MineAuction]")) {
+                        blockLocation.getWorld().getBlockAt(blockLocation).setType(Material.AIR);
+                        p.sendMessage(Messages.get("SIGN_REMOVED"));
+                        event.setCancelled(true);
                     }
                 }
             }
 
-
             if (Storer.playernames.contains(playerName)) {
-
                 Location blockLocation = block.getLocation();
-                Location upLoc = new Location(blockLocation.getWorld(), blockLocation.getX(), blockLocation.getY() + 1, blockLocation.getZ());
-                upLoc.getWorld().getBlockAt(upLoc).setType(Material.SIGN);
-                Sign sign = (Sign) upLoc.getWorld().getBlockAt(upLoc).getState();
+                Location upLoc = new Location(blockLocation.getWorld(), blockLocation.getBlockX(), blockLocation.getBlockY() + 1, blockLocation.getBlockZ());
+                Block upLocBlock = upLoc.getBlock();
+                upLocBlock.setType(Material.SIGN_POST);
+                org.bukkit.material.Sign matSign = new org.bukkit.material.Sign(Material.SIGN_POST);
+                matSign.setFacingDirection(Utils.getClosestFace((float) Math.toDegrees(Math.atan2(p.getLocation().getBlockX() - upLoc.getX(), upLoc.getZ() - p.getLocation().getBlockZ()))).getOppositeFace());
+                Sign sign = (Sign) upLocBlock.getState();
                 sign.setLine(0, "[MineAuction]");
                 sign.setLine(1, Messages.get("SIGN_OPEN"));
+                sign.setData(matSign);
                 sign.update();
                 p.sendMessage(Messages.get("SIGN_PLACED"));
                 Storer.playernames.remove(playerName);
             }
 
-            if (Storer.playernamesremove.contains(playerName)) {
-                Location blockLocation = block.getLocation();
-                Sign sign = (Sign) blockLocation.getWorld().getBlockAt(blockLocation).getState();
+            if (blockType == Material.SIGN_POST || blockType == Material.WALL_SIGN) {
+                Sign sign = (Sign) block.getState();
                 if (sign.getLine(0).equals("[MineAuction]")) {
-                    blockLocation.getWorld().getBlockAt(blockLocation).setType(Material.AIR);
-                    p.sendMessage(Messages.get("SIGN_REMOVED"));
+                    if (sign.getLine(1).equals(Messages.get("SIGN_OPEN"))) {
+                        p.openInventory(Storer.auctionMenu);
+                    }
                 }
             }
         }
